b2479a53d71a849ca026bbfa5b87b808f907cfb1
rommel dalisay <rommelmdalisay@gmail.com>
2023-09-25 11:24:56 +0800

    swipe refresh

--- app/src/main/java/com/example/itemcrud2023/ListItemActivity.java
+++ app/src/main/java/com/example/itemcrud2023/ListItemActivity.java
@@ -1,8 +1,10 @@
 package com.example.itemcrud2023;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -24,51 +26,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListItemActivity extends AppCompatActivity {
-    private static final String urlString ="http://192.168.1.11:8000/api/item" ;
+    private static final String urlString = "http://192.168.1.11:8000/api/item";
 
     private List<Item> item_list;
     private RecyclerView rv;
     private myAdapter adapter;
-
     Context mContext;
+    private SwipeRefreshLayout swipeContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         mContext = getApplicationContext();
 
-        rv= (RecyclerView) findViewById(R.id.itemrecyclerview);
+        rv = (RecyclerView) findViewById(R.id.itemrecyclerview);
         rv.setHasFixedSize(true);
         rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         item_list = new ArrayList<>();
-//        adapter = new myAdapter(mContext,item_list);
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
         getItemData();
+        swipeContainer.setOnRefreshListener(() -> {
+//            refreshData();
+            getItemData();
+        });
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
+
     private void getItemData() {
+        item_list.clear();
+
         JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET,
-                urlString,null, response -> {
-                    try {
-                        //JSONObject jsonObject=new JSONObject(response);
-                        //JSONArray array=jsonObject.getJSONArray("data");
-                        JSONArray array = response.getJSONArray("data");
-                        Log.i("data", array.toString());
-                        for (int i=0; i<array.length(); i++ ){
-                            JSONObject ob=array.getJSONObject(i);
-                            Item listData = new Item(ob.getString("description")
-                                    ,ob.getString("sell_price"),
-                                    ob.getString("cost_price"),
-                                    ob.getString("img_path"),
-                                    ob.getInt("item_id"));
-                            item_list.add(listData);
+                urlString, null, response -> {
+            try {
+                //JSONObject jsonObject=new JSONObject(response);
+                //JSONArray array=jsonObject.getJSONArray("data");
+                JSONArray array = response.getJSONArray("data");
+                Log.i("data", array.toString());
+                for (int i = 0; i < array.length(); i++) {
+                    JSONObject ob = array.getJSONObject(i);
+                    Item listData = new Item(ob.getString("description")
+                            , ob.getString("sell_price"),
+                            ob.getString("cost_price"),
+                            ob.getString("img_path"),
+                            ob.getInt("item_id"));
+                    item_list.add(listData);
 
-                        }
-                        adapter = new myAdapter(mContext,item_list);
-                        rv.setAdapter(adapter);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                }
+                adapter = new myAdapter(mContext, item_list);
+                rv.setAdapter(adapter);
+                swipeContainer.setRefreshing(false);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
-                }, error -> Log.e("error :","cannot get items")) {
+        }, error -> Log.e("error :", "cannot get items")) {
 //            @Override
 //            public Map<String, String> getHeaders() throws AuthFailureError {
 //                Map<String, String> params = new HashMap<String, String>();
@@ -80,7 +96,9 @@ public class ListItemActivity extends AppCompatActivity {
 
 
         };
-        RequestQueue requestQueue= Volley.newRequestQueue(mContext);
+        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
         requestQueue.add(jsonRequest);
     }
+
+
 }
